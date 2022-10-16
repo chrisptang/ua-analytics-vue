e0d4305af59239531e71ece85c5272635ff89716
pengtang <wholee@pengtangs-MacBook-Pro-13.local>
2022-10-16 16:25:35 +0800

    allowJs:true

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api-nsfw': {
-        target: 'http://127.0.0.1:5000',
+        target: 'http://127.0.0.1:5001',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api-nsfw/, '/')
       }
